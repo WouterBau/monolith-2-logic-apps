7aeaa745172d61aa1e88c85b4824e435db9f33eb
Wouter Bauchart <wouter.bauchart@outlook.com>
2022-04-16 16:46:15 +0200

    Added Phase 0 FTP Download part

--- Phase-0/Monolith2LogicApps0.Importer/ImportFunction.cs
+++ Phase-0/Monolith2LogicApps0.Importer/ImportFunction.cs
@@ -4,8 +4,8 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Azure.Storage.Blobs;
+using FluentFTP;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using Monolith2LogicApps0.Application.CsvConversion;
 using Monolith2LogicApps0.Domain;
@@ -14,16 +14,19 @@ namespace Importer
 {
     public class ImportFunction
     {
+
+        private const string LOCATION_NAME = "TypeA";
+
         [FunctionName("Function1")]
         [StorageAccount("AzureWebJobsStorage")]
         public async Task Run(
             [TimerTrigger("0 0 3 * * *")] TimerInfo myTimer,
-            [Blob("monolith2logicapps", FileAccess.ReadWrite)] BlobContainerClient blobContainerClient,
+            [Blob("import", FileAccess.ReadWrite, Connection = "Monolith2LogicAppsStorage")] BlobContainerClient blobContainerClient,
             [Sql("dbo.TypeAs", ConnectionStringSetting = "SqlConnection")] IAsyncCollector<TypeA> typeACollector,
             ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-            DownloadFiles();
+            await DownloadFiles(blobContainerClient, log);
             var items = await ReadItems(blobContainerClient, log);
             if (!items.Any())
                 return;
@@ -31,9 +34,21 @@ namespace Importer
             Report();
         }
 
-        private void DownloadFiles()
+        private async Task DownloadFiles(
+            BlobContainerClient blobContainerClient,
+            ILogger log)
         {
+            using var client = new FtpClient("127.0.0.1", "monolith2logicapps", "");
+            await client.AutoConnectAsync();
+            foreach (var item in await client.GetListingAsync("/" + LOCATION_NAME))
+            {
+                if (item.Type != FtpFileSystemObjectType.File)
+                    continue;
 
+                var data = await client.DownloadAsync(item.FullName, token: default);
+                await blobContainerClient.UploadBlobAsync(item.Name, new MemoryStream(data));
+                log.LogInformation($"File {item.Name} uploaded to Blob Storage");
+            }
         }
 
         private async Task<IEnumerable<TypeA>> ReadItems(
@@ -43,16 +58,16 @@ namespace Importer
             var items = new List<TypeA>();
             var reader = new CsvReader();
             var converter = new TypeACsvLineConverter();
-            await foreach (var blob in blobContainerClient
-                .GetBlobsAsync(
-                Azure.Storage.Blobs.Models.BlobTraits.All,
-                Azure.Storage.Blobs.Models.BlobStates.None,
-                "TypeAs/"))
+            await foreach (var blobItem in blobContainerClient.GetBlobsAsync(Azure.Storage.Blobs.Models.BlobTraits.All,
+                Azure.Storage.Blobs.Models.BlobStates.None, LOCATION_NAME + "/"))
             {
                 try
                 {
-                    var importedItems = reader.Parse(null, converter);
-                    items.AddRange(items);
+                    var blobClient = blobContainerClient.GetBlobClient(blobItem.Name);
+                    var blobDownload = await blobClient.DownloadContentAsync();
+                    var blobStream = blobDownload.Value.Content.ToStream();
+                    var importedItems = reader.Parse(blobStream, converter);
+                    items.AddRange(importedItems);
                 }
                 catch (Exception ex)
                 {
